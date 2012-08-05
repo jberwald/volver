60b95acc65fe203cdaf035a1bcc92507d02f3f51
Jesse Berwald <jberwald@gmail.com>
2012-08-05 13:47:26 -0400

    updating github gui's records

---
+++ README.md.bak
@@ -0,0 +1,4 @@
+volver
+======
+
+Poincare maps using CAPD library
---
+++ sandbox/template_ex.cpp
@@ -0,0 +1,76 @@
+// template_friend2.cpp
+// compile with: /EHsc
+#include <iostream>
+using namespace std;
+
+template <class T>
+class Array;
+
+template <class T>
+void f(Array<T>& a);
+
+template <class T> class Array
+{
+    T* array;
+    int size;
+
+public:
+    Array(int sz): size(sz)
+    {
+        array = new T[size];
+        memset(array, 0, size * sizeof(T));
+    }
+    Array(const Array& a)
+    {
+        size = a.size;
+        array = new T[size];
+        memcpy_s(array, a.array, sizeof(T));
+    }
+    T& operator[](int i)
+    {
+        return *(array + i);
+    }
+    int Length()
+    { 
+        return size;
+    }
+    void print()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            cout << *(array + i) << " ";
+        }
+        cout << endl;
+    }
+    // If you replace the friend declaration with the int-specific
+    // version, only the int specialization will be a friend.
+    // The code in the generic f will fail
+    // with C2248: 'Array<T>::size' :
+    // cannot access private member declared in class 'Array<T>'.
+    //friend void f<int>(Array<int>& a);
+
+    friend void f<>(Array<T>& a);
+};
+
+// f function template, friend of Array<T>
+template <class T>
+void f(Array<T>& a)
+{
+    cout << a.size << " generic" << endl;
+}
+
+// Specialization of f for int arrays
+// will be a friend because the template f is a friend.
+template<> void f(Array<int>& a)
+{
+    cout << a.size << " int" << endl;
+}
+
+int main()
+{
+    Array<char> ac(10);
+    f(ac);
+
+    Array<int> a(10);
+    f(a);
+}
